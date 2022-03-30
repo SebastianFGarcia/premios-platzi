6cafdec60e103ab55545fa907daa84317c0dd8f2
Sebastian Garcia <sfernandogarcia@uts.edu.co>
2022-03-30 09:52:30 -0500

    creando templates y pasando parametros en las vistas

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,detail,results,vote
 
 urlpatterns = [
     path('', index , name="index"),
+    path('<int:question_id>', detail, name="detail"),
+    path('results/<int:question_id>', results, name="results"),
+    path('vote/<int:question_id>', vote, name="vote"),
 ]
--- apps/polls/views.py
+++ apps/polls/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("hello")
+    latest_question_list = Question.objects.all()
+    
+    data = {
+        "latest_question_list":latest_question_list,
+    }
+    
+    return render(request, "polls/index.html",data)
+
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    data = {
+        "question":question
+    }
+    
+    return render(request, 'polls/detail.html', data)
+
+
+def results(request,question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero  {question_id}")
+
+
+def vote(request,question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero  {question_id}")
+    
